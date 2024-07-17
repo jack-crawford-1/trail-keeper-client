e2d88ead926a622ddb6f5960c8c6e0afef072dfc
Jack Crawford <primes_unit.05@icloud.com>
2024-07-18 03:17:46 +1200

    Update fetch from GeoJSON endpoint to show map markers from DOC NZ for tramping locations + Proj4 to convert coordinate format

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react'
 import { Loader } from '@googlemaps/js-api-loader'
+import proj4 from 'proj4'
 
 const containerStyle = {
   width: '100%',
@@ -7,10 +8,23 @@ const containerStyle = {
   borderRadius: '15px',
 }
 
+//TODO fix type errors
+
 // Tararua Forest Park
 const center = {
   lat: -40.867903,
   lng: 175.340083,
+}
+
+// using proj4 to swap the coords format to something google can use to show the markers
+const sourceProj = 'EPSG:2193'
+const destProj = 'EPSG:4326'
+
+if (!proj4.defs[sourceProj]) {
+  proj4.defs(
+    sourceProj,
+    '+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +datum=WGS84 +units=m +no_defs'
+  )
 }
 
 export default function Map() {
@@ -22,15 +36,16 @@ export default function Map() {
     })
 
     loader
-      .importLibrary('maps')
+      .load()
       .then(() => {
         if (mapRef.current && window.google && window.google.maps) {
           const map = new window.google.maps.Map(mapRef.current, {
             center,
-            zoom: 14,
-            mapTypeControl: false,
+            zoom: 12,
+            mapTypeControl: true,
           })
 
+          // Adding the Topo50 overlay
           const tileLayer = new window.google.maps.ImageMapType({
             getTileUrl: function (coord, zoom) {
               const url = `https://tiles-cdn.koordinates.com/services;key=309f0bd07902459798c646caf1f95048/tiles/v4/layer=50767/EPSG:3857/${zoom}/${coord.x}/${coord.y}.png`
@@ -43,6 +58,43 @@ export default function Map() {
           })
 
           map.overlayMapTypes.insertAt(0, tileLayer)
+
+          fetch('http://localhost:3000/v1/geojson', {
+            headers: {
+              'Cache-Control': 'no-cache',
+            },
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              data.features.forEach((feature) => {
+                if (feature.geometry.type === 'Point') {
+                  const [lat, lng] = feature.geometry.coordinates
+                  const [longitude, latitude] = proj4(sourceProj, destProj, [
+                    lat,
+                    lng,
+                  ])
+
+                  const marker = new window.google.maps.Marker({
+                    map,
+                    position: {
+                      lat: latitude,
+                      lng: longitude,
+                    },
+                    title: feature.properties.name,
+                    zIndex: 1000,
+                  })
+
+                  marker.addListener('click', () => {
+                    alert(feature.properties.name)
+                  })
+                }
+              })
+
+              map.data.addGeoJson(data)
+            })
+            .catch((error) => {
+              console.error('Error loading GeoJSON data:', error)
+            })
         } else {
           console.error('Google Maps API failed to load')
         }
